f1b570394c1ce26791969008cb627000bae1411a
Chew Boon Zhan <chewboonzhan@gmail.com>
2021-01-16 19:24:00 +0800

    Added refactoring for translator

---
+++ app/src/main/java/com/example/prototypeb/controller/translator/Translator_categories.java
@@ -0,0 +1,29 @@
+package com.example.prototypeb.controller.translator;
+
+import java.util.HashMap;
+
+public class Translator_categories {
+    private  String[] items;
+    private HashMap <Integer,String> items_with_number;
+    public Translator_categories(){
+        items = new String[]{"Adverbs","Alphabets", "Attachments","Numbers","Pronouns"};
+        init_hashmap();
+    }
+    private void init_hashmap(){
+        items_with_number = new HashMap <Integer,String>();
+        for(int i = 0;i<items.length;i++){
+            items_with_number.put(i,items[i].toLowerCase());
+        }
+    }
+
+    public String[] getItems() {
+        return items;
+    }
+
+    public HashMap<Integer, String> getItems_with_number() {
+        return items_with_number;
+    }
+    public String get_Beginning_Category(){
+        return items_with_number.get(0);
+    }
+}
