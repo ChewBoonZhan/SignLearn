de9eded5f3ee231fc2e280b8f48dc1f9cc57e639
Whistlemans <chewboonzhan@gmail.com>
2021-02-25 18:29:38 +0800

    Added buggy animated navbar

--- app/src/main/java/com/example/prototypeb/MainActivity.java
+++ app/src/main/java/com/example/prototypeb/MainActivity.java
@@ -34,15 +34,16 @@ import com.example.prototypeb.controller.file_connections.File_connections;
 import com.example.prototypeb.ui.game.GameFragment;
 import com.example.prototypeb.ui.lesson.LessonFragment;
 import com.example.prototypeb.ui.translator.TranslatorFragment;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 //import com.example.prototypeb.controller.file_connections.Categories;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView loading_logo_image;
     private TextView loading_text;
     private View body_components;
-    private BottomNavigationView nav_fragment;
+    private ChipNavigationBar nav_fragment;
 
-
+    private Fragment fragment;
     private static boolean ready = false;
 
     
@@ -88,13 +89,39 @@ public class MainActivity extends AppCompatActivity {
     private void init_background_scenes(){
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+        nav_fragment.setItemSelected(R.id.navigation_translator,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new TranslatorFragment()).commit();
+
+        nav_fragment.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.navigation_translator:
+                        fragment = new TranslatorFragment();
+                        break;
+                    case R.id.navigation_lesson:
+                        fragment = new LessonFragment();
+                        break;
+                    case R.id.navigation_game:
+                        fragment = new GameFragment();
+                        break;
+
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
+        /*
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_translator, R.id.navigation_lesson, R.id.navigation_game)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(nav_fragment, navController);
-
+        */
     }
     public static boolean get_first_loaded_or_not(){
         return ready;
