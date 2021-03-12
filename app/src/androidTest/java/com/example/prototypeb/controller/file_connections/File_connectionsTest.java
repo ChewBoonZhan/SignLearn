35f8f3574c204c5011dc88b091ff504d661cbe66
Whistlemans <chewboonzhan@gmail.com>
2021-03-12 08:44:09 +0800

    Fixed mockito dependency issue

---
+++ app/src/androidTest/java/com/example/prototypeb/controller/file_connections/File_connectionsTest.java
@@ -0,0 +1,63 @@
+package com.example.prototypeb.controller.file_connections;
+
+import android.content.Context;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.prototypeb.MainActivity;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Rule;
+
+public class File_connectionsTest extends TestCase {
+    @Rule
+    public ActivityScenarioRule mActivityRule =
+            new ActivityScenarioRule( MainActivity.class);
+    private Context mContext;
+    private File_connections file_connections;
+    @Before
+    public void setUp() throws Exception {
+
+        ActivityScenario activity = mActivityRule.getScenario();
+        activity.onActivity(activity1 -> {
+            mContext = activity1;
+
+        });
+
+        //waitFor(activity_here::hasWindowFocus);
+    }
+    public void testUnlock_category() {
+    }
+
+    public void testSave_lesson_not_scored_in_file() {
+    }
+
+    public void testSet_user_name() {
+    }
+
+    public void testSet_user_icon() {
+    }
+
+    public void testUpdate_score() {
+        int score = 10;
+        file_connections = new File_connections(mContext);
+        file_connections.update_score(score);
+        assertEquals(file_connections.getScore(),score);
+    }
+
+    public void testGetScore() {
+    }
+
+    public void testGet_user_name() {
+    }
+
+    public void testGet_user_icon() {
+    }
+
+    public void testGetSharedPref() {
+    }
+}
